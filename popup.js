2b591b2eaddd5aa8f51614821541d3bc87423e1a
Kuda <kudamusoni7@gmail.com>
2023-09-28 20:21:02 +0100

    clean up crew

--- popup.js
+++
@@ -1,9 +0,0 @@
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    if (tabs.length > 0) {
-        console.log(tabs);
-        var currentTab = tabs[0];
-        var currentUrl = currentTab.url;
-        console.log('Current URL:', currentUrl);
-        // chrome.runtime.sendMessage({ data: "Hello from the background!" });
-    }
-});
